92936bbfc912a627af4016febac93176ab4e6e32
Daniel Blanco <blueseph@gmail.com>
2016-12-04 00:58:30 -0500

    the game is afoot

--- textbasedgame/textbasedgame/textbasedgame.cpp
+++ textbasedgame/textbasedgame/textbasedgame.cpp
@@ -6,24 +6,15 @@
 #include <string>
 #include "Models/greeter/greeter.h"
 #include "Models/unit/unit.h"
+#include "Models\character_factory\characterfactory.h"
 
+#include <vector>
 using namespace std;
 
 void main()
 {
-	string myString;
-	string mySecondString;
-
-	Unit* unit = new Unit("user anth", 4, 5);
-	unit->move(5, 6);
-	cout << "we moved!" << endl;
-
-	Greeter* greeter = new Greeter();
-	greeter->askName();
-	greeter->sayGreeting();
-
-	cin >> myString;
-	cout << myString << endl;
-	cin >> mySecondString;
+	CharacterFactory* characterFactory = new CharacterFactory();
+	Unit* player = characterFactory->createHero();
+	vector<Unit*>* orcs = new vector<Unit*>();
+	orcs->push_back(characterFactory->createOrc());
 }
-
